1df02709ecb89c6e125341ae996c3c043b4c3ed0
Ramchandra-tharu <ramchandrachaudhary2003@gmail.com>
2025-02-03 23:57:13 +0530

    variable created

---
+++ Variable.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Variable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = 12;
+        int num2 = 14;
+        int sum = num2 + num1;
+        System.out.println(sum);
+
+        System.out.println("Enter your name : ");
+        String name = sc.nextLine();    // taking   (String) input from user .
+        System.out.println("your name is : " + name);
+
+        System.out.println("Enter  first number");
+        int n1 = sc.nextInt();
+        System.out.println("Enter second number");
+        int n2 = sc.nextInt();
+        int add = n1 + n2;
+        System.out.println("Sum of n1 and n2: " + add);
+
+
+
+
+    }
+}
