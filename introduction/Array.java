263821c4cc45ff5ef1e388ff2aafca8db0aed5db
Ramchandra-tharu <ramchandrachaudhary2003@gmail.com>
2025-02-07 23:12:26 +0530

    array  is created

---
+++ introduction/Array.java
@@ -0,0 +1,10 @@
+
+public class Array {
+    public static void main(String[] args) {
+        int marks [] = {23, 32, 65, 76, 65,}; // element of an array
+    for(int i = 0; i<marks.length; i++){
+        System.out.println(marks[i]); // print the elements
+    }
+
+    }
+}
