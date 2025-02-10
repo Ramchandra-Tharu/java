8a2eaaf30fc1768654fc669df0ea180c5951da78
Ramchandra-tharu <ramchandrachaudhary2003@gmail.com>
2025-02-10 22:26:46 +0530

    insertion sort is created

---
+++ introduction/Sort/Insertion.java
@@ -0,0 +1,26 @@
+package Sort;
+
+public class Insertion {
+    public static void PrintArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 2, 9, 1, 6, 4, 5};
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        PrintArray(arr);
+    }
+}
