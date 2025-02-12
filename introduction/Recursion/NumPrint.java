0c169d8b652abdf90463abf9e9cf3230ea6234b9
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-02-12 22:27:34 +0530

    Recursion file is created

---
+++ introduction/Recursion/NumPrint.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class NumPrint {
+    public static void Print(int n){
+        if(n == 11){
+            return;
+        }
+        System.out.println(n);
+        
+        Print(n+1);
+
+    }
+        
+    
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+        System.out.println("the output is:");
+        Print(n);
+    }
+    
+}
