6ac238f1982ae1a002eb4476e09fca5185922283
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-11-14 21:36:26 +0530

    in revision if else is clear

---
+++ introduction/Revision/If.java
@@ -0,0 +1,22 @@
+
+    
+import java.util.Scanner;
+public class If {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Enter you age: ");
+    int age = scan.nextInt();
+    if(age >=18){
+    System.out.println("you age is grater then 18  so you are eligiabble for vote");
+    }
+    else{
+    System.out.println("you are not eligible:");
+    }
+        
+    }
+    
+}
+
+    
+
