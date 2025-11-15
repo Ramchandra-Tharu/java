bf455a8264489cdb3e8e784d9172dd68b76daeea
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-11-15 23:58:20 +0530

    revision program is solve

---
+++ introduction/Revision/ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        int num = 39;
+    for(int i = 0; i<= 50; i++){
+        System.out.println(num);
+    }
+    }
+    
+}
