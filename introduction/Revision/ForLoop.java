8c61abc3e1cf46fd83a4b1e382b030603b208f3b
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-11-16 20:56:22 +0530

    updates

--- introduction/Revision/ForLoop.java
+++ introduction/Revision/ForLoop.java
@@ -1,8 +1,7 @@
 public class ForLoop {
     public static void main(String[] args) {
-        int num = 39;
     for(int i = 0; i<= 50; i++){
-        System.out.println(num);
+        System.out.println(i);
     }
     }
     
