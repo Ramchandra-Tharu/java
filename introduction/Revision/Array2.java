6432345799f6735ef98811ef341a6b053874acbc
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-11-28 00:07:56 +0530

    new program in revision

---
+++ introduction/Revision/Array2.java
@@ -0,0 +1,19 @@
+
+
+public class Array2 {
+    public static void main(String[] args) {
+        int array[] = {2,3,4,5,6,9};
+        for(int i = 0; i<array.length; i++){
+            if(i % 2 ==0){
+            System.out.println(i);
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+        
+      
+
+    }
+    
+}
