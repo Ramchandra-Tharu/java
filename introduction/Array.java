576bac67544514599d8578e6fd58e2b5515c0167
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2026-02-14 11:59:40 +0530

    Refactor Array class to calculate and print sum of elements; add new arrPrint class

--- introduction/Array.java
+++ introduction/Array.java
@@ -1,10 +1,12 @@
-
-public class Array {
+public class Array{
     public static void main(String[] args) {
-        int marks [] = {23, 32, 65, 76, 65,}; // element of an array
-    for(int i = 0; i<marks.length; i++){
-        System.out.println(marks[i]); // print the elements
-    }
+        int sum = 0;
 
-    }
+        int arr [] = {3, 4, 5, 6, 7, 8, 64, 3, 3, 54};
+
+        for( int i = 0; i<=arr.length; i ++){
+            sum += arr[i];
+            System.out.println("sum of arr is : " + sum);
+        }
+}
 }
